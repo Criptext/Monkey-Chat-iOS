e0058e2de74b44ed2869ef7821d7b4844f50fd56
Gianni Carlo <gcarlo89@hotmail.com>
2016-09-19 09:38:08 -0500

    Add Rotation Navigation to lock rotation on demand

---
+++ MonkeyChat/RotationNavigationController.swift
@@ -0,0 +1,18 @@
+//
+//  RotationNavigationController.swift
+//  MonkeyChat
+//
+//  Created by Gianni Carlo on 9/12/16.
+//  Copyright © 2016 Criptext. All rights reserved.
+//
+
+import UIKit
+
+class RotationNavigationController: UINavigationController {
+    
+    var lockAutorotate = false
+    
+    override func shouldAutorotate() -> Bool {
+        return !self.lockAutorotate
+    }
+}
