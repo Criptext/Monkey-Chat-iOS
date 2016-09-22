9d3b7cf65d66aef8a571841747bfc55049f9725a
aj1310 <aj_ortiz1310@icloud.com>
2016-09-21 21:12:00 -0500

    Ignore pods directory

--- Pods/Target Support Files/MonkeyKitUI/MonkeyKitUI-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MonkeyKitUI : NSObject
-@end
-@implementation PodsDummy_MonkeyKitUI
-@end
--- Pods/Target Support Files/Pods-MonkeyChat/Pods-MonkeyChat-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_MonkeyChatVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_MonkeyChatVersionString[];
-
