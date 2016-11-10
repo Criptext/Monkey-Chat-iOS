691d2cbf239f4be9f6bc4bb72a1d8b61bf029eef
Erika Perugachi <erikaperugachi@gmail.com>
2016-11-10 09:18:55 -0500

    Update App icon

---
+++ MonkeyChat/InfoConversationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  InfoConversationViewController.swift
+//  MonkeyChat
+//
+//  Created by Erika Perugachi on 11/9/16.
+//  Copyright © 2016 Criptext. All rights reserved.
+//
+
+import UIKit
+
+class InfoConversationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
